99d2a78ac640254eaccc60807fb64c24707afa49
Sander Wegter <pilot008@gmail.com>
2022-05-04 11:28:56 +0200

    Added color

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="text-white">
     <h1>Welcome </h1>This is the homepage
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./index.html",
@@ -21,6 +23,62 @@ module.exports = {
         "gray-200": "#D3D4D4",
         "gray-100": "#ECECEC",
         "gray-50": "#F4F4F4",
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: [
+            {
+              color: theme("colors.gray.200"),
+              '[class~="lead"]': {
+                color: theme("colors.gray.300"),
+              },
+              a: {
+                color: theme("colors.indigo.500"),
+              },
+              strong: {
+                color: theme("colors.gray.200"),
+              },
+              "ol > li::before": {
+                color: theme("colors.gray.400"),
+              },
+              "ul > li::before": {
+                backgroundColor: theme("colors.gray.600"),
+              },
+              hr: {
+                borderColor: theme("colors.gray.700"),
+              },
+              blockquote: {
+                color: theme("colors.gray.200"),
+                borderLeftColor: theme("colors.gray.600"),
+              },
+              h1: {
+                color: theme("colors.gray.100"),
+              },
+              h2: {
+                color: theme("colors.gray.100"),
+              },
+              h3: {
+                color: theme("colors.gray.100"),
+              },
+              h4: {
+                color: theme("colors.gray.100"),
+              },
+              "figure figcaption": {
+                color: theme("colors.gray.400"),
+              },
+              thead: {
+                color: theme("colors.white"),
+                borderBottomColor: theme("colors.gray.400"),
+              },
+              "tbody tr": {
+                borderBottomColor: theme("colors.gray.600"),
+              },
+            },
+          ],
+        },
+      }),
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
